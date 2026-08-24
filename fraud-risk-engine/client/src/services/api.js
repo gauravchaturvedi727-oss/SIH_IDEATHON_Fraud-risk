b1276dc2cc588b4590b4625dfbac6595571e933a
Gourav Chaturvedi <gauravchaturvedi727@gmail.com>
2026-08-25 05:15:05 +0530

    Update api.js

--- fraud-risk-engine/client/src/services/api.js
+++ fraud-risk-engine/client/src/services/api.js
@@ -1,35 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://dhanrakshak-backend-lpt8.onrender.com/api",
     timeout: 60000,
 });
 
-
 api.interceptors.request.use(
     (config) => {
-
-        const token =
-            localStorage.getItem("token");
-
+        const token = localStorage.getItem("token");
 
         if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`;
-
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
-
         return config;
-
     },
     (error) => {
-
         return Promise.reject(error);
-
     }
 );
-
 
 export default api;
